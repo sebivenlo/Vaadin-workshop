7901e04d7d86301bc052fde89dcb60d901472ac0
Ibrahim Kouzak <364207@student.fontys.nl>
2019-12-08 17:00:47 +0100

    adding demo

--- Vaadin-Exercise/vaadin-workshop/src/main/java/my/vaadin/ApplicationUI.java
+++ Vaadin-Exercise/vaadin-workshop/src/main/java/my/vaadin/ApplicationUI.java
@@ -5,6 +5,7 @@ import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import modules.Customer;
 import modules.CustomerService;
 
@@ -17,5 +18,14 @@ import java.util.List;
 public class ApplicationUI extends UI {
 
     @Override
-    protected void init(VaadinRequest request) {}
+    protected void init(VaadinRequest request) {
+        Label label = new Label("Welcome to Vaadin + Springboot");
+        label.setStyleName(ValoTheme.LABEL_H1);
+
+        VerticalLayout layout = new VerticalLayout();
+        layout.addComponent(label);
+        layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+
+        setContent(layout);
+    }
 }
